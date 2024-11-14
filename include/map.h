181944d65dfb2f201a356413beffc8eccfabaf17
Anssi Mäkinen <amakinen@student.hive.fi>
2024-11-14 18:42:07 +0200

    Implement simple map drawing
    Top-down orthographic and scaled/strecthed to window for now,
    transformations and clipping to be implemented...

---
+++ include/map.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 17:45:09 by amakinen          #+#    #+#             */
+/*   Updated: 2024/11/14 18:11:30 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_H
+# define MAP_H
+
+# include <stdint.h>
+# include "MLX42/MLX42.h"
+
+typedef struct s_map
+{
+	uint32_t	size_x;
+	uint32_t	size_y;
+	uint32_t	*z;
+	uint32_t	*color;
+}	t_map;
+
+void	draw_map(mlx_image_t *image, t_map *map);
+
+#endif
---
+++ src/map.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 17:47:30 by amakinen          #+#    #+#             */
+/*   Updated: 2024/11/14 18:40:00 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "map.h"
+#include "line.h"
+
+#define INSET 0.1f
+
+static t_point	make_point(t_map *map, uint32_t x, uint32_t y)
+{
+	return ((t_point){
+		INSET + (1.0f - INSET - INSET) * x / (map->size_x - 1),
+		INSET + (1.0f - INSET - INSET) * y / (map->size_y - 1),
+		map->color[y * map->size_x + x],
+	});
+}
+
+static void	draw_column(mlx_image_t *image, t_map *map, uint32_t x)
+{
+	uint32_t	y;
+	t_point		curr;
+	t_point		next;
+
+	y = 0;
+	next = make_point(map, x, y);
+	while (++y < map->size_y)
+	{
+		curr = next;
+		next = make_point(map, x, y);
+		draw_line(image, curr, next);
+	}
+}
+
+static void	draw_row(mlx_image_t *image, t_map *map, uint32_t y)
+{
+	uint32_t	x;
+	t_point		curr;
+	t_point		next;
+
+	x = 0;
+	next = make_point(map, x, y);
+	while (++x < map->size_x)
+	{
+		curr = next;
+		next = make_point(map, x, y);
+		draw_line(image, curr, next);
+	}
+}
+
+void	draw_map(mlx_image_t *image, t_map *map)
+{
+	uint32_t	i;
+
+	i = 0;
+	while (i < map->size_y)
+		draw_row(image, map, i++);
+	i = 0;
+	while (i < map->size_x)
+		draw_column(image, map, i++);
+}
