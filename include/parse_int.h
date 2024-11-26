0e5b1f73995ba1120c003890f223e00e50d85fcf
Anssi Mäkinen <amakinen@student.hive.fi>
2024-11-26 15:44:10 +0200

    Add number parsing utility

---
+++ include/parse_int.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_int.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 17:09:01 by amakinen          #+#    #+#             */
+/*   Updated: 2024/11/25 18:07:29 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_INT_H
+# define PARSE_INT_H
+
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdint.h>
+
+bool	parse_int32(
+			const char *str, size_t len, size_t *parsed_len, int32_t *value);
+bool	parse_uint32_hex(
+			const char *str, size_t len, size_t *parsed_len, uint32_t *value);
+
+#endif
---
+++ src/parse_int.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_int.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 17:11:52 by amakinen          #+#    #+#             */
+/*   Updated: 2024/11/25 20:07:51 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse_int.h"
+#include <stdint.h>
+
+bool	parse_int32(
+	const char *str, size_t str_len, size_t *len, int32_t *value)
+{
+	size_t	skip;
+	int32_t	sign;
+	int32_t	digit;
+
+	if (str_len == 0)
+		return (false);
+	skip = 0;
+	sign = 1;
+	if (str[0] == '-')
+		sign = -1;
+	if (str[0] == '-' || str[0] == '+')
+		skip = 1;
+	*len = skip;
+	*value = 0;
+	while (*len < str_len && str[*len] >= '0' && str[*len] <= '9')
+	{
+		digit = str[(*len)++] - '0';
+		if ((sign > 0 && *value > (INT32_MAX - digit) / 10)
+			|| (sign < 0 && *value < (INT32_MIN + digit) / 10))
+			return (false);
+		*value = *value * 10 + sign * digit;
+	}
+	return (*len > skip);
+}
+
+static bool	parse_hex_digit(const char c, uint32_t *parsed)
+{
+	if (c >= '0' && c <= '9')
+		*parsed = (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		*parsed = (c - 'a' + 10);
+	else if (c >= 'A' && c <= 'F')
+		*parsed = (c - 'A' + 10);
+	else
+		return (false);
+	return (true);
+}
+
+bool	parse_uint32_hex(
+	const char *str, size_t str_len, size_t *len, uint32_t *value)
+{
+	size_t		skip;
+	uint32_t	digit;
+
+	if (str_len == 0)
+		return (false);
+	skip = 0;
+	if (str[0] == '+')
+		skip = 1;
+	if (str_len > skip + 1 && str[skip] == '0' && str[skip + 1] == 'x')
+		skip += 2;
+	*len = skip;
+	*value = 0;
+	while (*len < str_len && parse_hex_digit(str[*len], &digit))
+	{
+		(*len)++;
+		if (*value > (UINT32_MAX - digit) / 16)
+			return (false);
+		*value = *value * 16 + digit;
+	}
+	return (*len > skip);
+}
