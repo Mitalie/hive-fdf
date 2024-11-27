4ae62c142a3515be4c8d078771128ed8dacff217
Anssi Mäkinen <amakinen@student.hive.fi>
2024-11-27 17:30:47 +0200

    Add horizontal rotation and default/reset to true isometric view

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:58:34 by amakinen          #+#    #+#             */
-/*   Updated: 2024/11/27 15:56:28 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/11/27 17:29:12 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ typedef struct s_data {
 	mlx_t		*mlx;
 	mlx_image_t	*image;
 	t_mesh		mesh;
-	float		angle_deg;
+	float		azimuth_deg;
+	float		elevation_deg;
 	bool		need_redraw;
 }	t_data;
 
@@ -40,16 +41,40 @@ static void	clear_image(mlx_image_t *image)
 	}
 }
 
-static void	draw_with_angle(mlx_image_t *image, t_mesh *mesh, float angle_rad)
+/*
+	1. shrink model and swap axes to match world XYZ
+	2. rotate horizontal plane
+	3. tilt and scale horizontal axis to match aspect ratio
+
+	Model size and aspect ratio should really be handled by separate projection
+	matrix (to be implemented)
+*/
+static void	draw_with_angle(mlx_image_t *image, t_mesh *mesh,
+	float azimuth_rad, float elevation_rad)
 {
 	t_mat4f	transform;
+	t_mat4f	next;
 
-	transform = ((t_mat4f){
-			(t_vec4f){{cosf(angle_rad), sinf(angle_rad), 0.0f, 0.0f}},
-			(t_vec4f){{-sinf(angle_rad), cosf(angle_rad), 0.0f, 0.0f}},
-			(t_vec4f){{0.0f, -0.5f, 1.0f, 0.0f}},
-			(t_vec4f){{0.0f, 0.0f, 0.0f, 1.0f}},
-		});
+	transform = (t_mat4f){
+		(t_vec4f){{1.0f / 20, 0.0f, 0.0f, 0.0f}},
+		(t_vec4f){{0.0f, 0.0f, -1.0f / 20, 0.0f}},
+		(t_vec4f){{0.0f, -1.0f / 20, 0.0f, 0.0f}},
+		(t_vec4f){{0.0f, 0.0f, 0.0f, 1.0f}},
+	};
+	next = (t_mat4f){
+		(t_vec4f){{cosf(azimuth_rad), 0.0f, sinf(azimuth_rad), 0.0f}},
+		(t_vec4f){{0.0f, 1.0f, 0.0f, 0.0f}},
+		(t_vec4f){{-sinf(azimuth_rad), 0.0f, cosf(azimuth_rad), 0.0f}},
+		(t_vec4f){{0.0f, 0.0f, 0.0f, 1.0f}},
+	};
+	transform = mul4f_mat_mat(&next, &transform);
+	next = (t_mat4f){
+		(t_vec4f){{(float)image->height / image->width, 0.0f, 0.0f, 0.0f}},
+		(t_vec4f){{0.0f, cosf(elevation_rad), sinf(elevation_rad), 0.0f}},
+		(t_vec4f){{0.0f, -sinf(elevation_rad), cosf(elevation_rad), 0.0f}},
+		(t_vec4f){{0.0f, 0.0f, 0.0f, 1.0f}},
+	};
+	transform = mul4f_mat_mat(&next, &transform);
 	clear_image(image);
 	draw_mesh(image, mesh, &transform);
 }
@@ -65,13 +90,21 @@ static void	key_hook(mlx_key_data_t key_data, void *param)
 		return ;
 	}
 	else if (key_data.key == MLX_KEY_R && key_data.action == MLX_PRESS)
-		fdf_data->angle_deg = -45;
-	else if (key_data.key == MLX_KEY_LEFT && (
-			key_data.action == MLX_PRESS || key_data.action == MLX_REPEAT))
-		fdf_data->angle_deg += 5;
-	else if (key_data.key == MLX_KEY_RIGHT && (
-			key_data.action == MLX_PRESS || key_data.action == MLX_REPEAT))
-		fdf_data->angle_deg -= 5;
+	{
+		fdf_data->azimuth_deg = -45;
+		fdf_data->elevation_deg = 35.2643897f;
+	}
+	else if (key_data.action == MLX_PRESS || key_data.action == MLX_REPEAT)
+	{
+		if (key_data.key == MLX_KEY_LEFT)
+			fdf_data->azimuth_deg += 5;
+		else if (key_data.key == MLX_KEY_RIGHT)
+			fdf_data->azimuth_deg -= 5;
+		else if (key_data.key == MLX_KEY_UP)
+			fdf_data->elevation_deg += 2;
+		else if (key_data.key == MLX_KEY_DOWN)
+			fdf_data->elevation_deg -= 2;
+	}
 	fdf_data->need_redraw = true;
 }
 
@@ -82,7 +115,8 @@ static void	loop_hook(void *param)
 	fdf_data = param;
 	if (fdf_data->need_redraw)
 		draw_with_angle(fdf_data->image, &fdf_data->mesh,
-			fdf_data->angle_deg / 180 * 3.1415926535);
+			fdf_data->azimuth_deg / 180 * 3.1415926535,
+			fdf_data->elevation_deg / 180 * 3.1415926535);
 	fdf_data->need_redraw = false;
 }
 
@@ -98,7 +132,8 @@ int	main(int argc, char **argv)
 	if (!data.mlx)
 		return (1);
 	data.image = mlx_new_image(data.mlx, data.mlx->width, data.mlx->height);
-	data.angle_deg = -45;
+	data.azimuth_deg = -45;
+	data.elevation_deg = 35.2643897f;
 	if (data.image)
 	{
 		mlx_key_hook(data.mlx, key_hook, &data);
