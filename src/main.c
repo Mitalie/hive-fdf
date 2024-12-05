eac78e0ed739fc619157602266792cd6fea3d8ff
Anssi Mäkinen <amakinen@student.hive.fi>
2024-12-05 21:58:15 +0200

    wip movable camera

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:58:34 by amakinen          #+#    #+#             */
-/*   Updated: 2024/12/05 19:29:19 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/12/05 21:56:55 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct s_data {
 	t_mesh		mesh;
 	float		azimuth_deg;
 	float		elevation_deg;
+	t_vec4		camera_pos;
 	bool		need_redraw;
 }	t_data;
 
@@ -41,19 +42,45 @@ static void	clear_image(mlx_image_t *image)
 	}
 }
 
-static void	draw_with_angle(mlx_image_t *image, t_mesh *mesh,
-	float azimuth_rad, float elevation_rad)
+static void	move_camera(t_data *data, float right, float up, float back)
 {
 	t_mat4	transform;
 	t_mat4	next;
+	float	azimuth_rad;
+	float	elevation_rad;
+
+	// angles and order inverted to rotate camera vector in world instead of world around camera position
+	elevation_rad = -data->elevation_deg / 180 * 3.1415926535;
+	transform = mat4(
+			vec4(1.0f, 0.0f, 0.0f, 0.0f),
+			vec4(0.0f, cosf(elevation_rad), -sinf(elevation_rad), 0.0f),
+			vec4(0.0f, sinf(elevation_rad), cosf(elevation_rad), 0.0f),
+			vec4(0.0f, 0.0f, 0.0f, 1.0f));
+	azimuth_rad = -data->azimuth_deg / 180 * 3.1415926535;
+	next = mat4(
+			vec4(cosf(azimuth_rad), 0.0f, sinf(azimuth_rad), 0.0f),
+			vec4(0.0f, 1.0f, 0.0f, 0.0f),
+			vec4(-sinf(azimuth_rad), 0.0f, cosf(azimuth_rad), 0.0f),
+			vec4(0.0f, 0.0f, 0.0f, 1.0f));
+	transform = mul_mm4(&next, &transform);
+	data->camera_pos = add4(data->camera_pos, mul_mv4(&transform, vec4(right, up, back, 1)));
+}
+
+static void	draw_with_angle(t_data *data)
+{
+	t_mat4	transform;
+	t_mat4	next;
+	float	azimuth_rad;
+	float	elevation_rad;
 
 	// move camera at (10,10,10) to origin
 	transform = mat4(
-			vec4(1.0f, 0.0f, 0.0f, -10.0f),
-			vec4(0.0f, 1.0f, 0.0f, -10.0f),
-			vec4(0.0f, 0.0f, 1.0f, -10.0f),
+			vec4(1.0f, 0.0f, 0.0f, -data->camera_pos.x),
+			vec4(0.0f, 1.0f, 0.0f, -data->camera_pos.y),
+			vec4(0.0f, 0.0f, 1.0f, -data->camera_pos.z),
 			vec4(0.0f, 0.0f, 0.0f, 1.0f));
 	// rotate camera in horizontal plane
+	azimuth_rad = data->azimuth_deg / 180 * 3.1415926535;
 	next = mat4(
 			vec4(cosf(azimuth_rad), 0.0f, sinf(azimuth_rad), 0.0f),
 			vec4(0.0f, 1.0f, 0.0f, 0.0f),
@@ -61,6 +88,7 @@ static void	draw_with_angle(mlx_image_t *image, t_mesh *mesh,
 			vec4(0.0f, 0.0f, 0.0f, 1.0f));
 	transform = mul_mm4(&next, &transform);
 	// rotate camera up/down
+	elevation_rad = data->elevation_deg / 180 * 3.1415926535;
 	next = mat4(
 			vec4(1.0f, 0.0f, 0.0f, 0.0f),
 			vec4(0.0f, cosf(elevation_rad), -sinf(elevation_rad), 0.0f),
@@ -70,13 +98,13 @@ static void	draw_with_angle(mlx_image_t *image, t_mesh *mesh,
 	// projection: adjust for vertical and horizontal field of view,
 	// put -z into w for perspective divide, w to z to store depth as -1/z
 	next = mat4(
-			vec4(2 * (float)image->height / image->width, 0.0f, 0.0f, 0.0f),
+			vec4(2 * (float)data->image->height / data->image->width, 0.0f, 0.0f, 0.0f),
 			vec4(0.0f, 2, 0.0f, 0.0f),
 			vec4(0.0f, 0.0f, 0.0f, 1.0f),
 			vec4(0.0f, 0.0f, -1.0f, 0.0f));
 	transform = mul_mm4(&next, &transform);
-	clear_image(image);
-	draw_mesh(image, mesh, &transform);
+	clear_image(data->image);
+	draw_mesh(data->image, &data->mesh, &transform);
 }
 
 static void	key_hook(mlx_key_data_t key_data, void *param)
@@ -93,6 +121,7 @@ static void	key_hook(mlx_key_data_t key_data, void *param)
 	{
 		fdf_data->azimuth_deg = -45;
 		fdf_data->elevation_deg = 35.2643897f;
+		fdf_data->camera_pos = vec4(10, 10, 10, 1);
 	}
 	else if (key_data.action == MLX_PRESS || key_data.action == MLX_REPEAT)
 	{
@@ -104,6 +133,18 @@ static void	key_hook(mlx_key_data_t key_data, void *param)
 			fdf_data->elevation_deg += 2;
 		else if (key_data.key == MLX_KEY_DOWN)
 			fdf_data->elevation_deg -= 2;
+		else if (key_data.key == MLX_KEY_W)
+			move_camera(fdf_data, 0, 0, -1);
+		else if (key_data.key == MLX_KEY_S)
+			move_camera(fdf_data, 0, 0, 1);
+		else if (key_data.key == MLX_KEY_A)
+			move_camera(fdf_data, -1, 0, 0);
+		else if (key_data.key == MLX_KEY_D)
+			move_camera(fdf_data, 1, 0, 0);
+		else if (key_data.key == MLX_KEY_Q)
+			move_camera(fdf_data, 0, 1, 0);
+		else if (key_data.key == MLX_KEY_Z)
+			move_camera(fdf_data, 0, -1, 0);
 	}
 	fdf_data->need_redraw = true;
 }
@@ -129,9 +170,7 @@ static void	loop_hook(void *param)
 		fdf_data->need_redraw = true;
 	}
 	if (fdf_data->need_redraw)
-		draw_with_angle(fdf_data->image, &fdf_data->mesh,
-			fdf_data->azimuth_deg / 180 * 3.1415926535,
-			fdf_data->elevation_deg / 180 * 3.1415926535);
+		draw_with_angle(fdf_data);
 	fdf_data->need_redraw = false;
 }
 
@@ -149,6 +188,7 @@ int	main(int argc, char **argv)
 	data.image = mlx_new_image(data.mlx, data.mlx->width, data.mlx->height);
 	data.azimuth_deg = -45;
 	data.elevation_deg = 35.2643897f;
+	data.camera_pos = vec4(10, 10, 10, 1);
 	if (data.image)
 	{
 		mlx_key_hook(data.mlx, key_hook, &data);
