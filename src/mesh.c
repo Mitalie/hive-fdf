b16237cf2cbda87e356fb1ab36273ba362051f30
Anssi Mäkinen <amakinen@student.hive.fi>
2024-12-16 16:15:06 +0200

    mesh: Extract combined transform helper function

--- src/mesh.c
+++ src/mesh.c
@@ -6,19 +6,15 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:29:17 by amakinen          #+#    #+#             */
-/*   Updated: 2024/12/16 15:14:46 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/12/16 16:13:56 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mesh.h"
 #include "line.h"
 
-void	draw_mesh(t_z_image *image, t_mesh *mesh, t_mat4 *transform)
+static t_mat4	mesh_combine_transform(t_mesh *mesh, t_mat4 *transform)
 {
-	uint32_t	i;
-	t_line		line;
-	t_vertex	a;
-	t_vertex	b;
 	t_mat4		mesh_transform;
 
 	mesh_transform = mat4(
@@ -26,15 +22,26 @@ void	draw_mesh(t_z_image *image, t_mesh *mesh, t_mat4 *transform)
 			vec4(0, mesh->scale.y, 0, mesh->pos.y),
 			vec4(0, 0, mesh->scale.z, mesh->pos.z),
 			vec4(0, 0, 0, 1));
-	mesh_transform = mul_mm4(transform, &mesh_transform);
+	return (mul_mm4(transform, &mesh_transform));
+}
+
+void	draw_mesh(t_z_image *image, t_mesh *mesh, t_mat4 *transform)
+{
+	uint32_t	i;
+	t_line		line;
+	t_vertex	a;
+	t_vertex	b;
+	t_mat4		combined_transform;
+
+	combined_transform = mesh_combine_transform(mesh, transform);
 	i = 0;
 	while (i < mesh->n_lines)
 	{
 		line = mesh->lines[i++];
 		a = mesh->vertices[line.a];
-		a.pos = mul_mv4(&mesh_transform, a.pos);
+		a.pos = mul_mv4(&combined_transform, a.pos);
 		b = mesh->vertices[line.b];
-		b.pos = mul_mv4(&mesh_transform, b.pos);
+		b.pos = mul_mv4(&combined_transform, b.pos);
 		draw_line(image, a, b);
 	}
 }
